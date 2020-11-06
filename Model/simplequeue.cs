e200cf29a045b09d65fc4fc756688be6c41e2b44
Sarklai <andv10@mail.ru>
2020-11-06 14:35:14 +0300

    v1

---
+++ Model/simplequeue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace queue_test.Model
+{
+    public class simplequeue<T>
+    {
+        private List<T> items = new List<T>();
+
+        private T Head => items.Last(); // Хвост
+
+        private T Tail => items.First(); // Голова
+
+        public int Count => items.Count; // Кол-во
+
+        public simplequeue() { }
+
+        public simplequeue(T data)
+        {
+            items.Add(data);
+        }
+
+        public void Enqueue (T data)
+        {
+            items.Insert(0, data);
+        }
+
+        public T Dequeue()
+        {
+            var item = Head;
+            items.Remove(item);
+            return item;
+        }
+
+        public T Peek()
+        {
+            return Head;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using queue_test.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace queue_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var simplequeue = new simplequeue<int>();
+            simplequeue.Enqueue(1);
+            simplequeue.Enqueue(2);
+            simplequeue.Enqueue(3);
+            simplequeue.Enqueue(4);
+            simplequeue.Enqueue(5);
+
+            Console.WriteLine(simplequeue.Dequeue());
+            Console.WriteLine(simplequeue.Peek());
+            Console.WriteLine(simplequeue.Dequeue());
+
+            Console.ReadLine();
+        }
+    }
+}
